9c24b0669dfe7e22ed54d4a65b537302953abcd9
StiveLucas <161142347+StiveLucas@users.noreply.github.com>
2025-05-07 01:57:37 +0000

    Roblox

---
+++ Tutorias/Lacos de repeticao.lua
@@ -0,0 +1,13 @@
+for x = 0, 10, 1 do
+	print(x)
+end
+
+-- No início do voce dar o valor da variavel, no meio coloca as vezes que irá repetir, ja no 
+-- final coloca em quantas casas ele vai pular.
+
+
+while condicao > 10 do
+    --codigo
+end
+
+--Esse é outro laço
---
+++ Tutorias/if e else.lua
@@ -0,0 +1,12 @@
+if condicao then
+	
+end
+
+-- Explicação: Apenas digite o if e a sua condicao, e dê Enther, o then e o end irão aparecer sozinho
+
+--Abaixo um exemplo usando elseif
+if condicao then
+	
+elseif condicao then
+	
+end
